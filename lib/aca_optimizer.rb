83a5991b3e15efd200721d747f509539874a209c
Trey Evans <lewis.r.evans@gmail.com>
2025-03-21 14:04:57 -0400

    Update method documentation.

--- lib/aca_optimizer.rb
+++ lib/aca_optimizer.rb
@@ -9,17 +9,17 @@ require_relative "aca_optimizer/aca_optimizer"
 module AcaOptimizer
   # @!parse ruby
   #
-  #  # Ruby facade for the Rust-provided RateCache class.
+  #  # Ruby facade for the Rust-provided {RateCache}[../crate_docs/aca_optimizer/rate_lookup/index.html] class.
   #  #
-  #  # The implementation of this object is actually provided by a Ruby extension
-  #  # written in rust.  This is merely the documentation presented to show Yard
-  #  # an API.
+  #  # This object is provided by the Ruby extension.
   #  class RateCache
-  #    # Create a new instance.
+  #    # Create a new instance - maps to
+  #    # {initialize}[../crate_docs/aca_optimizer/rate_lookup/types/struct.RateCache.html#method.initialize].
   #    def initialize
   #    end
   #
-  #    # Add a rate entry to the cache.
+  #    # Add a rate entry to the cache - maps to
+  #    # {cache_rate}[../crate_docs/aca_optimizer/rate_lookup/types/struct.RateCache.html#method.cache_rate].
   #    # @param product_id      [String] the id of the product
   #    # @param min_age         [Integer] the lowest age supported by this
   #    #                                  product's rate set
@@ -34,6 +34,8 @@ module AcaOptimizer
   #    # @param tobacco_use     [String | nil] tobacco usage value for the rate
   #    # @param cost            [Numeric] the cost of the rate with these
   #    #                                 parameters
+  #    # @return [void]
+  #    # @raise [ArgumentError] invalid arguments provided
   #    def add_rate(
   #      product_id,
   #      min_age,
@@ -47,7 +49,8 @@ module AcaOptimizer
   #    )
   #    end
   #
-  #    # Look up a rate.
+  #    # Look up a rate - maps to
+  #    # {lookup_rate}[../crate_docs/aca_optimizer/rate_lookup/types/struct.RateCache.html#method.lookup_rate].
   #    # @param product_id          [String] the id of the product
   #    # @param rating_area         [String] the rating area, as a string
   #    # @param rate_schedule_year  [Integer] the year of the rate schedule date
@@ -56,6 +59,8 @@ module AcaOptimizer
   #    # @param coverage_age        [Integer] the age on coverage start for the person
   #    # @param tobacco_use         [String | nil] tobacco usage for the person
   #    # @return [Double] the found cost
+  #    # @raise [ArgumentError] invalid arguments provided, or no
+  #    #   corresponding data found  
   #    def lookup_rate(
   #      product_id,
   #      rating_area,
@@ -67,11 +72,15 @@ module AcaOptimizer
   #    )
   #    end
   #
-  #    # Finish the loading of rates and compact memory.
+  #    # Finish the loading of rates and compact memory - maps to
+  #    # {finish_caching}[../crate_docs/aca_optimizer/rate_lookup/types/struct.RateCache.html#method.finish_caching]
+  #    # @return [void]
   #    def finish_caching!
   #    end
   #
-  #    # Return the cached class instance.  Populated at class load.
+  #    # Return the cached class instance - maps to
+  #    # {rate_cache_instance}[../crate_docs/aca_optimizer/rate_lookup/fn.rate_cache_instance.html].
+  #    # Populated at class load.
   #    # @return [RateCache] the cached instance
   #    def self.instance
   #    end
